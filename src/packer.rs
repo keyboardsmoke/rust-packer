a6ef072a0ef9c7ad5fe98d8c41f4ab8d95d949c6
Andrew Artz <9844465+keyboardsmoke@users.noreply.github.com>
2023-04-02 19:14:14 -0500

    * pack section

--- src/packer.rs
+++ src/packer.rs
@@ -1,9 +1,10 @@
 //cl#![warn(clippy::all)]
 
+use std::ops::Index;
 use std::path::PathBuf;
 use std::{io::Read, io::Write};
 use std::fs;
-use exe::{PE, Buffer};
+use exe::{PE, Buffer, ImageSectionHeader, SectionCharacteristics};
 mod encryption;
 
 #[path = "shared/mem.rs"]
@@ -32,36 +33,53 @@ pub fn pack(filename: PathBuf, output_filename: PathBuf, key: Vec<u8>) -> anyhow
     // let (head, body, tail) = unsafe { buffer.align_to::<IMAGE_DOS_HEADER>() };
     let mut pe = exe::pe::VecPE::from_data(exe::pe::PEType::Disk, buffer);
 
-    let dos = pe.get_valid_dos_header();
-    if dos.is_err() {
-        return Err(anyhow::Error::msg("Invalid DOS header."));
-    }
-
-    let dos_hdr = dos.unwrap().clone();
-    
+    let dos = pe.get_valid_dos_header()?;
+    let dos_hdr = dos.clone();
     if dos_hdr.e_magic.ne(&exe::DOS_SIGNATURE) {
         return Err(anyhow::Error::msg("Invalid DOS signature."));
     }
 
-    let nts = pe.get_valid_mut_nt_headers_64();
-    if nts.is_err() {
-        return Err(anyhow::Error::msg("Invalid NT header."));
-    }
-
-    // We don't need mutable.
-    let nts_hdr = nts.unwrap().clone();
-
+    let nts = pe.get_valid_nt_headers_64()?;
+    let nts_hdr = nts.clone();
     if nts_hdr.signature.ne(&exe::NT_SIGNATURE) {
         return Err(anyhow::Error::msg("Invalid NT signature."));
+    }
+
+    println!("Size of PE before 0x{:X}", pe.calculate_disk_size().unwrap());
+
+    // Append key to the end of the binary, which should be where our section is?...
+    for i in 0..0x1000 {
+        if i < key.len() {
+            let kv = *key.index(i);
+            pe.get_mut_buffer().append([kv]);
+        } else {
+            pe.get_mut_buffer().append([0]);
+        }
     }
 
     // Run steps
     encryption::pack(pe.clone(), dos_hdr, nts_hdr, key)?;
 
-    // Add metadata
-    // let mut newHdr: ImageSectionHeader;
-    // newHdr.set_name(Some(".pack"));
-    // pe.append_section(section)
+    let chars = SectionCharacteristics::MEM_READ | SectionCharacteristics::ALIGN_4096BYTES | SectionCharacteristics::CNT_INITIALIZED_DATA;
+
+    let new_hdr = ImageSectionHeader { 
+        name: [exe::CChar(0x2E), exe::CChar(0x70), exe::CChar(0x61), exe::CChar(0x63), exe::CChar(0x6B), exe::CChar(0x00), exe::CChar(0x00), exe::CChar(0x00)], 
+        virtual_size: 0x1000, 
+        virtual_address: exe::RVA(0), 
+        size_of_raw_data: 0x1000, 
+        pointer_to_raw_data: exe::Offset(0), 
+        pointer_to_relocations: exe::Offset(0), 
+        pointer_to_linenumbers: exe::Offset(0), 
+        number_of_relocations: 0, 
+        number_of_linenumbers: 0, 
+        characteristics: chars
+    };
+
+    pe.append_section(&new_hdr)?;
+
+    pe.fix_image_size()?;
+
+    println!("Size of PE after 0x{:X}", pe.calculate_disk_size().unwrap());
 
     let writable = pe.get_mut_buffer();
 
--- src/unpacker/encryption.rs
+++ src/unpacker/encryption.rs
@@ -1,6 +1,6 @@
-use std::borrow::BorrowMut;
+use std::{borrow::BorrowMut};
 
-use exe::{ImageDOSHeader, ImageNTHeaders64};
+use exe::{ImageDOSHeader, ImageNTHeaders64, PE};
 
 #[path = "../shared/mem.rs"]
 mod mem;
@@ -10,14 +10,23 @@ mod section;
 
 pub fn initialize()
 {
-    //
+    todo!("Nothing here yet.");
 }
 
 pub fn run(_base: u64, mut pe: exe::pe::PtrPE, _peb: u64, dos: &ImageDOSHeader, nts: &ImageNTHeaders64) -> anyhow::Result<(), anyhow::Error>
 {
-    // Static key for now.
-    let key: [u8; 3] = [0x50, 0xBE, 0x17];
-    println!("Warning: Using static development key.");
+    let pack_section_hdr = pe.get_section_by_name(".pack")?;
+    let pack_section_data = pack_section_hdr.read(&pe)?;
+    let pack_section_vec = pack_section_data.to_vec();
+
+    let mut spl = pack_section_vec.split(|num| *num == 0);
+    let key_split = spl.next().ok_or(anyhow::Error::msg("Unable to find key data in section"))?;
+
+    print!("Pack key read [");
+    for i in 0..key_split.len() {
+        print!("{:X}", key_split[i]);
+    }
+    println!("]");
 
     section::foreach_section_module(pe.borrow_mut(), *dos, *nts, |ptr, sec| {
         let s = sec.first().unwrap();
@@ -36,7 +45,7 @@ pub fn run(_base: u64, mut pe: exe::pe::PtrPE, _peb: u64, dos: &ImageDOSHeader, 
 
         for i in start .. end as usize {
             let key_index = i - start;
-            let kv = key[key_index.rem_euclid(key.len())];
+            let kv = key_split[key_index.rem_euclid(key_split.len())];
             unsafe {
                 let base: *mut u8 = ptr.add(i);
                 *base = *base ^ kv;
